7eb428d338053ab016415728c4c2d3cd97e1dbe6
Manuel Castanon-Puga <puga@uabc.edu.mx>
2022-10-17 09:47:07 -0700

    Esta es una nueva nueva version

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
 }
